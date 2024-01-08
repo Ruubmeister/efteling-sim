0bedfeafe99f924ce65a9daad0619c2937576228
Ruben Lelieveld <ruben@rubium.nl>
2024-01-08 19:38:11 +0100

    Finish all grid positions

--- frontend/src/helpers.tsx
+++ frontend/src/helpers.tsx
@@ -1,16 +1,16 @@
 
 const upperLeftCorner = {
-    "lat": 1.0,
-    "lon": 1.0
+    "lat": 51.65325,
+    "lon": 5.04505
 }
 
 const bottomRightCorner = {
-    "lat": 1.0,
-    "lon": 1.0
+    "lat": 51.64641,
+    "lon": 5.05403
 }
 
-const gridYCount = 300;
-const gridXCount = 300;
+const gridYCount = 322;
+const gridXCount = 262;
 
 const xStep = bottomRightCorner["lat"] - upperLeftCorner["lat"] / gridXCount;
 const yStep = bottomRightCorner["lon"] - upperLeftCorner["lon"] / gridYCount;
