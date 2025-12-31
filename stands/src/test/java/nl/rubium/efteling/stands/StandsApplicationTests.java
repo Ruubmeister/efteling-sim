740a17ecd8dea2d49b3ed4bd2814bd7c0d1a0ddb
Ruben Lelieveld <ruben.lelieveld@vanderlande.com>
2025-12-31 10:19:20 +0100

    Fix missing kafka consumer for park and some other small things

--- stands/src/test/java/nl/rubium/efteling/stands/StandsApplicationTests.java
+++ stands/src/test/java/nl/rubium/efteling/stands/StandsApplicationTests.java
@@ -3,11 +3,11 @@ package nl.rubium.efteling.stands;
 import nl.rubium.efteling.common.location.control.LocationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class StandsApplicationTests {
-    @MockBean private LocationService locationService;
+    @MockitoBean private LocationService locationService;
 
     @Test
     void contextLoads() {}
